fb485b56f2f9209072f07fbdb404e6a7e107fa95
Sarang <myid9595@gmail.com>
2026-07-10 13:36:27 -0700

    adding more files

--- Tuple.py
+++ Tuple.py
@@ -1,7 +1,13 @@
 #Tuple is similar to a list, but it is immutable.
+#Tuples are an ordered collection, meaning they maintain the insertion order of the elements
 
-tuple = ("Sarang","Sambharia")
-print(tuple)
+user_tuple = ("Sarang","Sambharia")
+print(user_tuple)
 
-#Below line will throw an error as assignment is not allower in a tuple
-tuple[0] = "Sambharia"
+#Below line will throw an error as assignment is not allowed in a tuple.
+#this also prevents for the tuple to be accidentally modified.
+user_tuple[0] = "Sambharia"
+
+#We can cast a tuple into list like below
+user_list = list(user_tuple)
+print(user_list)
--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class practice:
     #Print Fizz for a number multiple of 3, Buzz for multiple of 5 and FizzBuzz for multiple of both
     def fizzbuzz(self):
@@ -42,7 +45,77 @@ class practice:
         print("The maximum number from the list of numbers is:",max(list_of_numbers))
         print("The minimum number from the list of number is:", min(list_of_numbers))
 
+    #Create a new list from an existing list but with just unique elements
+    def unique_elements_list(self,list):
+        my_set = set(list)
+        print(my_set)
+
+    #Count the frequency of all the characters in a given string
+    def character_counter(self,user_input):
+        dict = {}
+        for char in user_input:
+            dict[char] = dict.get(char,0) + 1
+        print(dict)
+
+    #get the common elements from 2 lists and put it in a third list
+    def find_common(self,list1, list2):
+        set1 = set(list1)
+        set2 = set(list2)
+        output_list = list(set1.intersection(set2))
+        print(output_list)
+
+    #pallindrome or not?
+    def pallindrome(self,user_string):
+        #this line means we are string from back, taking -1 steps(in reverse).
+        #we can also do it like: user_string[-1::-1]
+        string_reverse = user_string[::-1]
+        if string_reverse == user_string:
+            print("Pallindrome")
+        else:
+            print("Not Pallindrome")
+
+    #Factorial calculator
+    def calculate_factorial(self, num):
+        result = 1
+        for i in range(1,num+1):
+            result *= i
+        print(result)
+
+    #This method is for practice and try outs
+    def practice_method(self):
+        name = "sarang"
+        print(name[-1::-1])
+
+    def valid_paranthesis(self, s: str) -> bool:
+        stack = deque()
+        open_set = {'(', '[', '{'}
+        closed_set = {')', ']', '}'}
+        for char in s:
+            if char in open_set:
+                stack.append(char)
+            if char in closed_set:
+                if len(stack) == 0:
+                    return False
+                if char == ')' and stack[-1] == '(':
+                    stack.pop()
+                    continue
+
+                if char == ']' and stack[-1] == '[':
+                    stack.pop()
+                    continue
+
+                if char == '}' and stack[-1] == '{':
+                    stack.pop()
+                    continue
+                else:
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
 
 obj = practice()
-list_of_numbers = [1,2,3,4,5]
-obj.find_in_list(list_of_numbers)
+
+s = "()"
+print(obj.valid_paranthesis(s))
